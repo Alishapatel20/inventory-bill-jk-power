8a94b5d523a3d909f16d568dcae01b1ab30ac25d
Aalisha <alisha.palminfotech@gmail.com>
2025-12-24 00:49:32 +0530

    pdf

--- src/invetory/supabaseClient.js
+++ src/invetory/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "https://nxyfigbmgisszqrlndvx.supabase.co";
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "sb_publishable_V2xVtiVrfr4HahyWSwF7CA_A4pwyClq";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
